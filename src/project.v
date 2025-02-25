4cbcdae20bdeb0a8a8c8cba326694f1945f3bb25
Srinivas Harish <srinivas7992@gmail.com>
2025-02-25 18:00:38 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,52 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
 
 module tt_um_l2 (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+    input  wire [7:0] ui_in,   
+    input  wire [7:0] uio_in,  
+    output wire [7:0] uo_out,  
+    input  wire       ena,     
+    input  wire       clk,     
+    input  wire       rst_n,   
+    output wire [7:0] uio_out, 
+    output wire [7:0] uio_oe   
+`ifdef GL_TEST
+    ,input wire VPWR,          
+    input wire VGND            
+`endif
 );
+ 
+    wire [15:0] in;
+    assign in = {ui_in, uio_in};  // ui_in is A[7:0] (MSB), uio_in is B[7:0] (LSB)
+ 
+    reg [7:0] out;
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    // Priority encoder logic
+    always @(*) begin
+        if (in[15])         out = 8'd15;  
+        else if (in[14])    out = 8'd14;
+        else if (in[13])    out = 8'd13;
+        else if (in[12])    out = 8'd12;
+        else if (in[11])    out = 8'd11;
+        else if (in[10])    out = 8'd10;
+        else if (in[9])     out = 8'd9;
+        else if (in[8])     out = 8'd8;
+        else if (in[7])     out = 8'd7;
+        else if (in[6])     out = 8'd6;
+        else if (in[5])     out = 8'd5;
+        else if (in[4])     out = 8'd4;
+        else if (in[3])     out = 8'd3;
+        else if (in[2])     out = 8'd2;
+        else if (in[1])     out = 8'd1;
+        else if (in[0])     out = 8'd0;
+        else                out = 8'b1111_0000;  
+    end
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  
+    assign uo_out = (ena & rst_n) ? out : 8'b0;
+ 
+    assign uio_oe  = 8'b0;   
+    assign uio_out = 8'b0;   
 
 endmodule
+
+`default_nettype wire
