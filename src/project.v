e1445c5fe12292c6e2f5571809b2dfc375d3e4a3
Srinivas Harish <srinivas7992@gmail.com>
2025-02-24 23:59:30 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_8_bit_alu (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
