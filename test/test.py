f703a14899f76ad68b0a09d5aee707d58f45eb80
Srinivas Harish <srinivas7992@gmail.com>
2025-02-25 19:55:09 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
  
+
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -10,7 +11,7 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
+ 
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
@@ -39,6 +40,6 @@ async def test_project(dut):
     dut.ui_in.value = 0b10101010   
     dut.uio_in.value = 0b00000111  
     await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0b11010101, f"Expected 11010101, got {dut.uo_out.value}"
+    assert dut.uo_out.value == 0b01010101, f"Expected 01010101, got {dut.uo_out.value}"
 
     dut._log.info("All tests passed!")
