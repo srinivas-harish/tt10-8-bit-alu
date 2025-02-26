a20be7ecb0be1f7dc7831c409541ac21d242a735
Srinivas Harish <srinivas7992@gmail.com>
2025-02-25 19:34:42 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -8,12 +8,10 @@ from cocotb.triggers import ClockCycles
 @cocotb.test()
 async def test_project(dut):
     dut._log.info("Start")
-
-   
+ 
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
-
-    # Reset
+ 
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
@@ -24,27 +22,27 @@ async def test_project(dut):
 
     dut._log.info("Test Bitwise Rotation Selector behavior")
 
-    # Test  
-    dut.ui_in.value = 0b10101010   
-    dut.uio_in.value = 0b00000000  
+    # Test   
+    dut.ui_in.value = 0b10101010    
+    dut.uio_in.value = 0b00000000   
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0b10101010, f"Expected 10101010, got {dut.uo_out.value}"
 
-    # Test 
-    dut.ui_in.value = 0b10101010   
-    dut.uio_in.value = 0b00000001  
+    # Test  
+    dut.ui_in.value = 0b10101010    
+    dut.uio_in.value = 0b00000001   
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0b01010101, f"Expected 01010101, got {dut.uo_out.value}"
 
     # Test  
-    dut.ui_in.value = 0b10101010   
-    dut.uio_in.value = 0b00000011  
+    dut.ui_in.value = 0b10101010     
+    dut.uio_in.value = 0b00000011    
     await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 0b01010101, f"Expected 01010101, got {dut.uo_out.value}"
+    assert dut.uo_out.value == 0b01010101, f"Expected 10101010, got {dut.uo_out.value}"
 
     # Test  
-    dut.ui_in.value = 0b10101010   
-    dut.uio_in.value = 0b00000111  
+    dut.ui_in.value = 0b10101010     
+    dut.uio_in.value = 0b00000111    
     await ClockCycles(dut.clk, 1)
     assert dut.uo_out.value == 0b11010101, f"Expected 11010101, got {dut.uo_out.value}"
 
